2bd66120935f6e69ef872a06dedeec54cb04b4aa
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-28 21:03:41 +0300

    Create test.rs

---
+++ tests/test.rs
@@ -0,0 +1,31 @@
+use lifering::FloatingPointScalars;
+
+#[test]
+#[should_panic]
+fn test_f64_nan() {
+    FloatingPointScalars::new(f64::NAN).unwrap();
+}
+
+#[test]
+#[should_panic]
+fn test_f32_nan() {
+    FloatingPointScalars::new(f32::NAN).unwrap();
+}
+
+#[test]
+fn test_value_recovery_f64() {
+    const PI: f64 = 3.141592653589793;
+    const EULER: f64 = 2.718281828459045;
+
+    assert_eq!(FloatingPointScalars::new(PI).unwrap().as_f64(), PI);
+    assert_eq!(FloatingPointScalars::new(EULER).unwrap().as_f64(), EULER);
+}
+
+#[test]
+fn test_value_recovery_f32() {
+    const PI: f32 = 3.14159265;
+    const EULER: f32 = 2.71828185;
+
+    assert_eq!(FloatingPointScalars::new(PI).unwrap().as_f32(), PI);
+    assert_eq!(FloatingPointScalars::new(EULER).unwrap().as_f32(), EULER);
+}
