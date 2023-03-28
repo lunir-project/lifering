5a4d32a6394eadc85a8660a4476bf1de48a40ac2
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-28 21:07:21 +0300

    Update test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,15 @@
-use lifering::FloatingPointScalars;
+use lifering::FloatingPointComponents;
 
 #[test]
 #[should_panic]
 fn test_f64_nan() {
-    FloatingPointScalars::new(f64::NAN).unwrap();
+    FloatingPointComponents::new(f64::NAN).unwrap();
 }
 
 #[test]
 #[should_panic]
 fn test_f32_nan() {
-    FloatingPointScalars::new(f32::NAN).unwrap();
+    FloatingPointComponents::new(f32::NAN).unwrap();
 }
 
 #[test]
@@ -17,8 +17,8 @@ fn test_value_recovery_f64() {
     const PI: f64 = 3.141592653589793;
     const EULER: f64 = 2.718281828459045;
 
-    assert_eq!(FloatingPointScalars::new(PI).unwrap().as_f64(), PI);
-    assert_eq!(FloatingPointScalars::new(EULER).unwrap().as_f64(), EULER);
+    assert_eq!(FloatingPointComponents::new(PI).unwrap().as_f64(), PI);
+    assert_eq!(FloatingPointComponents::new(EULER).unwrap().as_f64(), EULER);
 }
 
 #[test]
@@ -26,6 +26,6 @@ fn test_value_recovery_f32() {
     const PI: f32 = 3.14159265;
     const EULER: f32 = 2.71828185;
 
-    assert_eq!(FloatingPointScalars::new(PI).unwrap().as_f32(), PI);
-    assert_eq!(FloatingPointScalars::new(EULER).unwrap().as_f32(), EULER);
+    assert_eq!(FloatingPointComponents::new(PI).unwrap().as_f32(), PI);
+    assert_eq!(FloatingPointComponents::new(EULER).unwrap().as_f32(), EULER);
 }
